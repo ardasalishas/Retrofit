dccdeb2d5ae456d850d3b83fcb508cd83563edb1
ardasalisha <ardasalisha@gmail.com>
2021-07-13 16:33:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/retrofit/Post.java
@@ -0,0 +1,31 @@
+package com.example.retrofit;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    private int userId;
+    private int id;
+    private String title;
+    @SerializedName("body")
+    private String text;
+    private String comments;
+
+    public Post(int i, String new_title, String new_text) {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getText() {
+        return text;
+    }
+    public String getComments() {
+        return comments;
+    }
+}
